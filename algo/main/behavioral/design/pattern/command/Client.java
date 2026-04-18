213a853cfb82f0ab5545b0c96080526945601e57
lorioj <79774338+lorioj@users.noreply.github.com>
2026-04-18 14:10:42 +0800

    Add Javadoc comments to Client class
    Added documentation for the Client class.

--- algo/main/behavioral/design/pattern/command/Client.java
+++ algo/main/behavioral/design/pattern/command/Client.java
@@ -1,5 +1,9 @@
 package main.behavioral.design.pattern.command;
 
+/**
+ * Encapsulates a request as an object.
+ * Use when: implementing undo/redo, queues, or task scheduling.
+ */
 public class Client {
 
 	public static void main(String[] args) {
